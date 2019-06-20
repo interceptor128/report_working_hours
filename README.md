159f1b51b299d4d157aef43b1c98ae8f6cedc016
unknown <LX19010021@win.sharp.co.jp>
2019-06-20 15:33:32 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,46 @@
+Report working hours
+==
+
+# 概要
+
+このツールは作業実績報告書等を添付してメール送信することができます。
+
+報告を時短するために作成しました。
+
+## 動作に必要なもの
+
+本体プログラムはPythonでコーディングしているため、Pythonの実行環境が必要です。
+
++ [Python](https://www.python.org/)
+
+WindowsアプリケーションのAPIを使用するため、以下のライブラリが必要です。
+
++ [pywin32](https://github.com/mhammond/pywin32)
+
+リアルタイムにキーボード入力を処理するため、以下のライブラリが必要です。
+
++ [readchar](https://pypi.org/project/readchar/)
+
+## プログラムのダウンロード
+`git clone https://github.com/interceptor128/report_working_hours.git`  
+でレポジトリをクローン(ローカルPCにダウンロード)できます。
+
+## 使い方
+
+宛先(To,Cc,Bcc)の設定をテキストファイルに記載してください。
+設定例は`sample.conf`にあります。
+
+以下のコマンド入力でメール送信画面を表示またはメール送信します。
+```command:sample
+python report_working_hours.py sample.conf 資料.xls
+```
+ 
+設定ファイルと添付ファイルはpyファイルと同じディレクトリ（フォルダ）に入れて指定してください。
+
+必要に応じて、ソース内のメール内容を編集してください。  
+宛先の名前、差出人の名前等
+
+## 課題
++ To Cc Bcc には1アドレスしか指定できないので改善したい
++ 件名、メール内容の変更はソースコード内にハードコーディングしているためカスタマイズできない
+    + 許容範囲内？
---
+++ report_working_hours.py
@@ -0,0 +1,74 @@
+import win32com.client
+import datetime
+import time
+import sys
+import os
+import readchar
+
+args = sys.argv
+path = os.getcwd()
+
+def exit_proc():
+    key = readchar.readkey()
+    if not key:
+        sys.exit()
+
+def sendmail():
+    # Outlookのオブジェクト設定
+    outlook = win32com.client.Dispatch("Outlook.Application")
+    mymail = outlook.CreateItem(0)
+
+    # 日付の取得
+    vdate = datetime.date.today() # 今日の日付
+    vdatefc = vdate.strftime("%Y/%m/%d") # 変換後
+    dayoftheweek = ["月","火","水","木","金","土","日"]
+    yobi = dayoftheweek[vdate.weekday()]
+
+    #メールアドレスの設定
+    with open(confpath) as f:
+        address = [s.strip() for s in f.readlines()]
+    mymail.BodyFormat = 1               #テキストタイプ
+    if len(address) == 0:
+        print("宛先が未設定です。いずれかのキーを押すと終了します")
+        exit_proc()
+    elif len(address) == 1:
+        mymail.To = address[0]          # To
+    elif len(address) == 2:
+        mymail.To = address[0]          # To
+        mymail.cc = address[1]          # Cc
+    elif len(address) == 3:
+        mymail.To = address[0]          # To
+        mymail.cc = address[1]          # Cc
+        mymail.Bcc = address[2]         # Bcc
+
+    mymail.Subject = "【勤怠】 " + str(vdatefc) + " 分の送付"
+    mymail.Body = "〇〇さん" + "\n\n" + "お疲れ様です。□□です。" + "\n\n" + str(vdatefc) + "(" + yobi + ")" + "の勤怠を記載しましたので、\n" + "送付致します。" + "\n\n" + "以上、よろしくお願い致します。"
+
+    # ファイルが添付されている場合
+    if len(filepath) > 0:
+        mymail.Attachments.Add (filepath)
+
+    # メール送信
+    mymail.Display(True)    # 作成画面を表示
+    #mymail.Send()          # 画面非表示で送信
+
+if len(args) == 1:
+    print("設定ファイル、添付ファイルが指定されていません。いずれかのキーを押すと終了します")
+    exit_proc()
+elif len(args) == 2:
+    print("添付ファイル名が指定されていません。")
+    print("そのまま送信する場合は y、終了する場合は y 以外を入力してください")
+    key = readchar.readkey()
+    if not key == 'y':
+        sys.exit()
+    else:
+        confname = sys.argv[1]
+        confpath = path + '//' + confname
+        filepath = ''
+        sendmail()
+elif len(args) == 3:
+    confname = sys.argv[1]
+    confpath = path + '//' + confname
+    filename = sys.argv[2]
+    filepath = path + '//' + filename
+    sendmail()
