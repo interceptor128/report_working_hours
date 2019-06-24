7949281a9cdaf38e4fbf91ff68160b610d3cfd3b
interceptor128 <polestar.interceptor.0128@gmail.com>
2019-06-24 13:32:45 +0000

    PEP8 code edit

--- report_working_hours.py
+++ report_working_hours.py
@@ -1,6 +1,5 @@
 import win32com.client
 import datetime
-import time
 import sys
 import os
 import readchar
@@ -8,10 +7,12 @@ import readchar
 args = sys.argv
 path = os.getcwd()
 
+
 def exit_proc():
     key = readchar.readkey()
     if not key:
         sys.exit()
+
 
 def sendmail():
     # Outlookのオブジェクト設定
@@ -19,15 +20,15 @@ def sendmail():
     mymail = outlook.CreateItem(0)
 
     # 日付の取得
-    vdate = datetime.date.today() # 今日の日付
-    vdatefc = vdate.strftime("%Y/%m/%d") # 変換後
-    dayoftheweek = ["月","火","水","木","金","土","日"]
+    vdate = datetime.date.today()  # 今日の日付
+    vdatefc = vdate.strftime("%Y/%m/%d")  # 変換後
+    dayoftheweek = ["月", "火", "水", "木", "金", "土", "日"]
     yobi = dayoftheweek[vdate.weekday()]
 
-    #メールアドレスの設定
+    # メールアドレスの設定
     with open(confpath) as f:
         address = [s.strip() for s in f.readlines()]
-    mymail.BodyFormat = 1               #テキストタイプ
+    mymail.BodyFormat = 1               # テキストタイプ
     if 0 <= len(address) <= 1:
         print("宛先が未設定です。いずれかのキーを押すと終了します")
         exit_proc()
@@ -42,17 +43,21 @@ def sendmail():
         mymail.Bcc = address[3]         # Bcc
 
     mymail.Subject = "【勤怠】 " + str(vdatefc) + " 分の送付"
-    mymail.Body = "〇〇さん" + "\n\n" + "お疲れ様です。□□です。" + "\n\n" + str(vdatefc) + "(" + yobi + ")" + "の勤怠を記載しましたので、\n" + "送付致します。" + "\n\n" + "以上、よろしくお願い致します。"
+    valuebody1 = "〇〇さん" + "\n\n" + "お疲れ様です。□□です。" + "\n\n"
+    valuebody2 = str(vdatefc) + "(" + yobi + ")" + "の勤怠を記載しましたので、\n"
+    valuebody3 = "送付致します。" + "\n\n" + "以上、よろしくお願い致します。"
+    mymail.Body = valuebody1 + valuebody2 + valuebody3
 
     # ファイルが添付されている場合
     if len(filepath) > 0:
-        mymail.Attachments.Add (filepath)
+        mymail.Attachments.Add(filepath)
 
     # メール送信
     if address[1] == 0:
         mymail.Send()          # 画面非表示で送信
     else:
         mymail.Display(True)    # 作成画面を表示
+
 
 if len(args) == 1:
     print("設定ファイル、添付ファイルが指定されていません。いずれかのキーを押すと終了します")
